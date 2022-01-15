0c1703fae75a65439ff0aea9e20e34d7723bb62e
Ibrahim Ayman Abd Elwahab Alaswar <eng.ibrahim.cer@hotmail.com>
2022-01-15 10:44:24 +0200

    Shopping.Client

---
+++ Shopping/Shopping.Api/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.Api.Data;
+using Shopping.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get() {
+            return ProductContext.Products;
+        }
+
+    }
+}
